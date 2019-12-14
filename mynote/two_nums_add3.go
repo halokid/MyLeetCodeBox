0e6d0f402e55dc2858ab6c0a04d43b763bece0a3
mm jxx <r00txxyyzz@gmail.com>
2019-12-14 21:14:55 +0800

    tmp code

---
+++ mynote/two_nums_add3.go
@@ -0,0 +1,90 @@
+package main
+
+import (
+  "fmt"
+)
+
+type ListNode struct {
+  Val     int
+  Next    *ListNode
+}
+
+/**
+输入：(2 -> 4 -> 3) + (5 -> 6 -> 4)
+输出：7 -> 0 -> 8
+原因：342 + 465 = 807
+ */
+func addTwoNumbers(l1 *ListNode, l2 *ListNode) *ListNode {
+  l3 := ListNode{0, nil}
+  p := l1
+  q := l2
+  curr := &l3
+  carry := 0
+  for {
+    var x, y int
+
+    if p == nil && q == nil {
+      break
+    }
+
+    if p != nil {
+      x = p.Val
+    } else {
+      x = 0
+    }
+
+    if q != nil {
+      y = q.Val
+    } else {
+      y = 0
+    }
+
+    sum := x + y + carry
+    carry = sum / 10      // 如果 sum大于10， 则carry为1, 否则carry为0
+
+    // 下面的计算方式是一种计算模型， 不断的用自身去代替下一个计算对象
+    curr.Next = &ListNode{sum % 10, nil}
+    curr = curr.Next
+
+    if p != nil { p = p.Next }
+    if q != nil { q = q.Next }
+  }
+
+  if carry > 0 {
+    curr.Next = &ListNode{carry, nil}
+  }
+
+  return l3.Next      // 第一次赋值curr 的时候改了 l3 的next， 后面就是直接改 curr本身的了，当然也是改l3链上的，所以返回 l3.Next 是可以一直追下去的
+}
+
+func main() {
+  //l1C := ListNode{3, nil}
+  //l1B := ListNode{4, &l1C}
+  //l1A := ListNode{2, &l1B}
+
+  //l2C := ListNode{4, nil}
+  //l2B := ListNode{6, &l2C}
+  //l2A := ListNode{5, &l2B}
+
+  l1A := ListNode{3, nil}
+  l2A := ListNode{5, nil}
+  //fmt.Println(addTwoNumbers(&l1A, &l2A))
+
+  l3 := addTwoNumbers(&l1A, &l2A)
+  var curr *ListNode
+  curr = l3
+  for {
+    if curr.Next == nil {
+      fmt.Println(curr)
+      break
+    }
+
+    if curr.Next != nil {
+      fmt.Println(curr)
+      curr = curr.Next
+    }
+  }
+}
+
+
+
--- tmp/c1.go
+++ tmp/c1.go
@@ -1,61 +1,42 @@
 package main
 
-import (
-  "fmt"
-)
+import "fmt"
 
 type ListNode struct {
   Val     int
   Next    *ListNode
 }
 
-func addTwoNumbers(l1 *ListNode, l2 *ListNode) *ListNode {
-  var l *ListNode = &ListNode{}
-  pre := l
-  carry := 0
-  for l1 != nil || l2 != nil {
-    fmt.Println("pre ----------", pre)
-    pre.Next = &ListNode{}
-    p := pre.Next
-    x := 0
-    y := 0
-    if l1 != nil {
-      x = l1.Val
-      l1 = l1.Next
+func deleteNode(head *ListNode, node *ListNode) {
+  fmt.Println("head -----------", head)
+  l := &ListNode{Next: head}
+  //l := &ListNode{}
+  //l = head
+  for l.Next != nil {
+    fmt.Println("start loop ------------")
+    if l.Next.Val == node.Val {
+      fmt.Println("hit val -----------", l, l.Next)
+      //head = head.Next
+      if node.Val == head.Val {     // 如果是第一个元素
+        head = head.Next
+      }
+      l.Next = l.Next.Next
+      break
     }
-
-    if l2 != nil {
-      y = l2.Val
-      l2 = l2.Next
-    }
-
-    p.Val = (x + y + carry) % 10
-    fmt.Println("val ----------", p.Val)
-    carry = (x + y + carry) / 10
-    pre = p
-    fmt.Println("p ----------", p)
-    fmt.Println("++++++++++++++++++++++++++++++++++++++")
+    l = l.Next
   }
-
-  if carry != 0 {
-    pre.Next = &ListNode{Val: carry}
-  }
-  return l.Next
+  fmt.Println("done loop --------------- ")
+  fmt.Println(head, head.Next, head.Next.Next, head.Next.Next.Next)
+  fmt.Println(l, l.Next )
 }
 
 func main() {
-  l3 := ListNode{3, nil}
+  l4 := ListNode{5, nil}
+  l3 := ListNode{3, &l4}
   l2 := ListNode{4, &l3}
   l1 := ListNode{2, &l2}
 
-  lx3 := ListNode{4, nil}
-  lx2 := ListNode{6, &lx3}
-  lx1 := ListNode{5, &lx2}
-
-  ln := addTwoNumbers(&l1, &lx1)
-  fmt.Println(ln)
-  fmt.Println(ln.Next)
-  fmt.Println(ln.Next.Next)
+  node := ListNode{Val: 3}
+  //node := ListNode{Val: 2}
+  deleteNode(&l1, &node)
 }
-
-
