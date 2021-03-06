a609c0e0424ed65fb9cb7163acaacb9760eb4921
mm jxx <r00txxyyzz@gmail.com>
2021-03-06 21:10:35 +0800

    rust two sample

---
+++ best_sample/rust/src/main.rs
@@ -0,0 +1,6 @@
+mod two_sum;
+mod reverse_list;
+
+fn main() {
+    println!("Hello, world!");
+}
---
+++ best_sample/rust/src/reverse_list.rs
@@ -0,0 +1,36 @@
+
+#[derive(PartialEq, Eq, Clone, Debug)]
+pub struct ListNode {
+  val:    i32,
+  next:   Option<Box<ListNode>>     // 有可能是null， 所以要用option
+}
+
+impl ListNode {
+
+  #[inline]
+  fn new(val: i32) -> Self {
+    ListNode {
+      val:    val,
+      next:   None,
+    }
+  }
+}
+
+impl Solution {
+  pub fn reverse_list(head: Option<Box<ListNode>>) -> Option<Box<ListNode>> {
+    if head.is_none() {
+      return None;
+    }
+    let mut prev: Option<Box<ListNode>> = None;
+    let mut curr: Option<Box<ListNode>> = head;
+    while curr.is_some() {
+      let mut node = curr.take().unwrap();
+      curr = node.next;
+      node.next = prev;
+      prev = Some(node);
+    }
+    prev
+  }
+}
+
+
