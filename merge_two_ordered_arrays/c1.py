87134e955ce464fc4fae95507fbee93f83f3e034
codylee1234 <sc23chsl@leeds.ac.uk>
2024-06-06 23:53:59 +0800

    done merge two ordered arrays

--- merge_two_ordered_arrays/c1.py
+++ merge_two_ordered_arrays/c1.py
@@ -5,3 +5,16 @@ class Solution:
     """
     Do not return anything, modify nums1 in-place instead.
     """
+    l1 = m - 1
+    l2 = n - 1
+    l3 = m + n - 1
+
+    while l2 >= 0:
+      if l1 >= 0 and nums1[l1] > nums2[l2]:
+        nums1[l3] = nums1[l1]
+        l1 -= 1
+      else:
+        nums1[l3] = nums2[l2]
+        l2 -= 1
+      l3 -= 1
+
